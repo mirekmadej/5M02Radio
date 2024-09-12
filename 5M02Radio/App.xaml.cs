27717dcf09b88c3104242e4d1e636cd5002c53dd
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-09-12 10:09:15 +0200

    Dodaj pliki projektów.

---
+++ 5M02Radio/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _5M02Radio
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ 5M02Radio/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace _5M02Radio
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUstaw(object sender, EventArgs e)
+        {
+            if (rbBlue.IsChecked)
+                lblNapis.TextColor = Colors.Blue;
+            else if (rbGreen.IsChecked)
+                lblNapis.TextColor= Colors.Green;
+            else if (rbRed.IsChecked)
+                lblNapis.TextColor=( Colors.Red);   
+        }
+    }
+
+}
